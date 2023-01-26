77e91edf8bf320c26b3fde7d4547dcd9959aadf7
Gustavo <guuhnog@gmail.com>
2023-01-26 18:55:06 -0300

    first Commit

---
+++ src/App.vue
@@ -0,0 +1,334 @@
+<template>
+<div class="principal">
+  <div class="menu">
+    <img src="@/assets/disney.svg" alt="baner disney">
+    <div>
+        <div class="genero">
+        Genero
+      </div> 
+      <div class="prgf">
+        Animação
+      </div>
+      <div class="prgf">
+        Ação
+      </div>
+      <div class="prgf">
+        Terror
+      </div>
+      <div class="prgf">
+        Romance
+      </div>
+      <div class="prgf">
+        Comedia
+      </div>
+      <div class="prgf">
+        Velho Oeste
+      </div>
+    </div>
+    <div>  
+      <div class="prgfAnimacao">
+        Estudio de Animação
+      </div>
+      <div class="prgf logo">
+         <img src="@/assets/pixarlogo.svg"   alt="logo pixar">  Pixar
+      </div>
+      <div class="prgf  logo">
+          <img src="@/assets/marvellogo.svg" alt="logo marvel"> Marvel
+      </div>
+      <div class="prgf logo">
+        <img src="@/assets/illumiLogo.png" alt="logo Illumination" class="img"> Ilumination
+      </div>
+      <div class="prgf logo">
+        <img src="@/assets/logodw.png" alt="logo DreamWorks" class="img"> DreamWorks
+      </div>
+      <div class="prgf logo">
+        <img src="@/assets/wblogo.png" alt="logo Warner Bros" class="img"> Warner Bros
+      </div>
+      <div class="prgf logo">
+        <img src="@/assets/sonyLogo.png" alt="logo Sony Pictures" class="img">Sony Pictures
+      </div>
+    </div>
+  </div>
+  <div class="banner">
+    <img src="@/assets/marvel.svg" alt="baner da marvel" class="marvel">
+    <div class="top">
+      Top Novidades 
+    </div>
+    <div class="filmes">
+      <!-- homem aranha 2 -->
+      <div>   
+        <img src="@/assets/spiderman2.svg" alt="baner spiderman 2"> 
+        <div class="legendaCartaz">
+          <div class="alturaConteiner"> 
+            Homem-Aranha: <br>
+            Sem Volta Pra Casa
+          </div>
+          <div class="cartaz">
+            <img src="@/assets/estrela.svg" alt="estrela">
+            <img src="@/assets/estrela.svg" alt="estrela">
+            <img src="@/assets/estrela.svg" alt="estrela">
+            <img src="@/assets/estrela.svg" alt="estrela">
+            4,8
+          </div>
+        </div>
+      </div>
+      <!-- shang chi  -->
+      <div>
+        <img src="@/assets/shangchi.svg"  alt="baner shang-chi"> 
+        <div class="legendaCartaz">
+          <div class="alturaConteiner"> 
+            Shang-Chi
+          </div>
+          <div class="cartaz">
+            <img src="@/assets/estrela.svg" alt="estrela">
+            <img src="@/assets/estrela.svg" alt="estrela">
+            <img src="@/assets/estrela.svg" alt="estrela">
+            <img src="@/assets/estrela.svg" alt="estrela">
+            4,6
+          </div>
+        </div>
+      </div>
+      <!-- eternos-->
+      <div>
+        <img src="@/assets/eternos.svg"  alt="baner eternos ">
+        <div class="legendaCartaz">
+          <div class="alturaConteiner"> 
+            Eternos
+          </div>
+          <div class="cartaz">
+            <img src="@/assets/estrela.svg" alt="estrela">
+            <img src="@/assets/estrela.svg" alt="estrela">
+            <img src="@/assets/estrela.svg" alt="estrela">
+            3.1
+          </div>
+        </div>
+      </div>
+      <!-- pantera -->
+      <div>
+        <img src="@/assets/pantera.svg"  alt="baner pantera">
+        <div class="legendaCartaz">
+          <div class="alturaConteiner"> 
+            Pantera Negra
+          </div>
+          <div class="cartaz">
+            <img src="@/assets/estrela.svg" alt="estrela">
+            <img src="@/assets/estrela.svg" alt="estrela">
+            <img src="@/assets/estrela.svg" alt="estrela">
+            <img src="@/assets/estrela.svg" alt="estrela">
+            4,9
+          </div>
+        </div>
+      </div>
+      <!-- kingsman-->
+      <div>
+        <img src="@/assets/kingsman.svg"  alt="baner kingsman">
+        <div class="legendaCartaz">
+          <div class="alturaConteiner"> 
+            Kingsman: <br>
+            O Círculo Dourado
+          </div>
+          <div class="cartaz">
+            <img src="@/assets/estrela.svg" alt="estrela">
+            <img src="@/assets/estrela.svg" alt="estrela">
+            <img src="@/assets/estrela.svg" alt="estrela">
+            <img src="@/assets/estrela.svg" alt="estrela">
+            4,5
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="filmes">
+      <!-- homem aranha -->
+      <div>
+        <img src="@/assets/spiderman1.svg"  alt="baner spiderman 1 ">
+        <div class="legendaCartaz">
+          <div class="alturaConteiner"> 
+            Homem-Aranha: <br>
+            Longe de Casa
+          </div>
+          <div class="cartaz">
+            <img src="@/assets/estrela.svg" alt="estrela">
+            <img src="@/assets/estrela.svg" alt="estrela">
+            <img src="@/assets/estrela.svg" alt="estrela">
+            <img src="@/assets/estrela.svg" alt="estrela">
+            4,1
+          </div>
+        </div>
+      </div>
+      <!--vingadores-->
+      <div>
+        <img src="@/assets/vingadores.svg"  alt="baner vingadores ">
+        <div class="legendaCartaz">
+          <div class="alturaConteiner"> 
+            Vingadores: <br>
+            Ultimato
+          </div>
+          <div class="cartaz">
+            <img src="@/assets/estrela.svg" alt="estrela">
+            <img src="@/assets/estrela.svg" alt="estrela">
+            <img src="@/assets/estrela.svg" alt="estrela">
+            <img src="@/assets/estrela.svg" alt="estrela">
+            4,8
+          </div>
+        </div>
+      </div>
+      <!-- xmen-->
+      <div>
+        <img src="@/assets/xmen.svg"  alt="baner x-men">
+        <div class="legendaCartaz">
+          <div class="alturaConteiner"> 
+            X-Men: <br>
+            Fênix Negra
+          </div>
+          <div class="cartaz">
+            <img src="@/assets/estrela.svg" alt="estrela">
+            <img src="@/assets/estrela.svg" alt="estrela">
+            <img src="@/assets/estrela.svg" alt="estrela">
+            2.9
+          </div>
+        </div>
+      </div>
+      <!-- deadpool-->
+      <div>
+        <img src="@/assets/deadpool.svg"  alt="baner deadpool">
+        <div class="legendaCartaz">
+          <div class="alturaConteiner"> 
+            Deadpool
+          </div>
+          <div class="cartaz">
+            <img src="@/assets/estrela.svg" alt="estrela">
+            <img src="@/assets/estrela.svg" alt="estrela">
+            <img src="@/assets/estrela.svg" alt="estrela">
+            <img src="@/assets/estrela.svg" alt="estrela">
+            4,7
+          </div>
+        </div>
+      </div>
+      <!-- esquadrao suicida-->
+      <div>
+        <img src="@/assets/esquadrao.svg"  alt="baner esquadrao">
+        <div class="legendaCartaz">
+          <div class="alturaConteiner"> 
+            Esquadrão Suícida
+          </div>
+          <div class="cartaz">
+            <img src="@/assets/estrela.svg" alt="estrela">
+            <img src="@/assets/estrela.svg" alt="estrela">
+            <img src="@/assets/estrela.svg" alt="estrela">
+            3,7
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>  
+</div>
+
+</template>
+<style>
+body {
+  margin: 0;
+  padding: 0;
+}
+.principal {
+  background-color: black;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  border: blue 1px solid;
+  overflow: auto;
+}
+.menu {
+  width: 30vw;
+  height: 50vh;
+  padding: 4% 0 0 2.5%;
+  margin-left: 3%;
+}
+.genero {
+  color: white;
+  font-family: 'Roboto', sans-serif;
+  font-weight: 700;
+  font-size: 18px;
+  line-height: 21px;
+  margin: 10% 0 11% 0;
+}
+.genero:hover{
+  font-size: 20px;
+  color: #6495ED;
+  cursor: pointer;
+}
+.prgf {
+  color: white;
+  margin: 5% 0  6% 0;
+  font-weight: 500;
+  font-family: 'Roboto', sans-serif;
+  size: 14px;
+  line-height: 20px;
+}
+.prgf:hover {
+  font-size: 20px;
+  color: #6495ED;
+  cursor: pointer;
+}
+.logo {
+  display: flex;
+  align-items: center;
+  gap: 20px;
+}
+.prgfAnimacao {
+  color: white;
+  font-family: 'Roboto', sans-serif;
+  font-weight: 700;
+  font-size: 18px;
+  line-height: 21px;
+  margin: 20% 0 15% 0;
+}
+.prgfAnimacao:hover {
+  font-size: 20px;
+  cursor: pointer;
+  color: #6495ED;
+}
+.img {
+  width: 30px;
+  height: 30px;
+  border-radius: 50%;
+  background-color: white;
+}
+.marvel {
+  width: 70vw;
+  border-radius: 0 0 0 10px;
+}
+.top {
+  color: white;
+  font-weight: 700;
+  font-family: 'Roboto', sans-serif;
+  font-size: 25px;
+  margin: 3% 0 3% 5%;
+}
+.filmes {
+  display: flex;
+  gap: 40px;
+  margin: 8% 0 8% 5%;
+  
+}
+.legendaCartaz {
+  color: white;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+}
+.alturaConteiner {
+  height: 35px;
+}
+.legendaCartaz:hover {
+  cursor: pointer;
+  color: #6495ED;
+}
+.cartaz {
+  display: flex;
+  gap: 10px;
+  color: white;
+  margin-top: 5px;
+  align-items: center;
+}
+</style>
